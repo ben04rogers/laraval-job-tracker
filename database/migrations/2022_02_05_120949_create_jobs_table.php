f805415c8f65a935d98a8a8d64495054381f2980
Ben Rogers <47819009+ben04rogers@users.noreply.github.com>
2022-02-05 22:17:05 +1000

    Create migration for jobs table

---
+++ database/migrations/2022_02_05_120949_create_jobs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->id();
+            $table->string("company_name");
+            $table->dateTime("date_applied");
+            $table->string("job_title");
+            $table->string("post_url");
+            $table->integer("salary")->unsigned();
+            $table->string("status");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jobs');
+    }
+}
