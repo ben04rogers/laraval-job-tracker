7df67c578ee69cdc283c611b5165d825525abfbc
Ben Rogers <47819009+ben04rogers@users.noreply.github.com>
2022-02-05 22:07:45 +1000

    Flash message when logged out

--- app/Http/Controllers/Auth/LogoutController.php
+++ app/Http/Controllers/Auth/LogoutController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Session;
 
 class LogoutController extends Controller
 {
     public function store() {
         auth()->logout();
+
+        Session::flash('message', 'Successfully logged out'); 
 
         return redirect()->route("home");
     }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,10 @@
 @extends("layouts.app")
 
 @section("content")
-    <div>
-        <h1>This is the home page</h1>
+    <div class="py-3">
+        @if(Session::has('message'))
+        <p class="alert alert-info mb-2">{{ Session::get('message') }}</p>
+        @endif
+        <h1>Home page</h1>
     </div>
 @endsection
