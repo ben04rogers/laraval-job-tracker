011c3477fabadc50b9812962dd703c78b3e03e67
Ben Rogers <47819009+ben04rogers@users.noreply.github.com>
2022-02-10 19:23:21 +1000

    update padding on jumbotron home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,14 +5,14 @@
         @if(Session::has('message'))
         <p class="alert alert-info mb-2">{{ Session::get('message') }}</p>
         @endif
-        <div class="container mt-4 bg-light rounded p-5 mb-5">
+        <div class="mt-4 bg-light rounded py-5 mb-5">
             <div class="row align-items-center">
               <div class="col-md-7">
                 <h1>Manage Your Job Applications</h1>
                 <p>JobTrack helps you keep track of all your job applications in one place.</p>
                 <a class="btn btn-primary" href="{{ route("register") }}" role="button">Sign Up</a>
               </div>
-          
+
               <div class="col-md-5">
                 <img class="img-fluid" src="/img/home.svg">
               </div>
