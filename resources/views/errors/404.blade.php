817e6ea2ce66dc124be7a138de38e2b9abac02a3
Ben Rogers <47819009+ben04rogers@users.noreply.github.com>
2022-02-12 09:38:49 +1000

    Add 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,14 @@
+@extends("layouts.app")
+
+@section("content")
+    <div class="row align-items-center">
+        <div class="col-md-6">
+            <h2 class="mb-4">Oops! Page cannot be found</h2>
+            <p>Navigate back to <a href="/">home screen</a>.</p>
+        </div>
+        
+        <div class="col-md-6">
+            
+        </div>
+  </div>
+@endsection
