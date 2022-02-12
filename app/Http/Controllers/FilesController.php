c46312cfd74b92e44587c5a7430a7e1fc8ee33b3
Ben Rogers <47819009+ben04rogers@users.noreply.github.com>
2022-02-12 22:11:46 +1000

    Users can only see files they have uploaded

--- app/Http/Controllers/FilesController.php
+++ app/Http/Controllers/FilesController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\File;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 
 class FilesController extends Controller
 {
     public function index() {
-        $files = File::all();
+        $files =  DB::table("files")->get()->where("user_id", Auth::user()->id);
 
         return view("files", compact('files'));
     }
