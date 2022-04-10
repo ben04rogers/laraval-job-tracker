2757b5c513992e65a51849ac9c58adba0029ff2b
Ben Rogers <benrogers1299@outlook.com>
2022-04-10 14:19:56 +1000

    Seperature file list into blade component

---
+++ resources/views/components/files-list.blade.php
@@ -0,0 +1,26 @@
+@props(['files'])
+<div class="all-files py-4">
+    @if (!$files->isEmpty())
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th scope="col">File</th>
+                <th scope="col"></th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach ($files as $file)
+                <tr>
+                    <td class="">{{ $file->file }}</td>
+                    <td class="d-flex justify-content-end">
+                        <a class="btn btn-outline-success mx-2" href="{{ url("/files/view", $file->id) }}">View</a>
+                        <a class="btn btn-outline-primary" href="{{ url("/files/download", $file->file) }}">Download</a>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    @else
+        <p>No files have been uploaded.</p>
+    @endif
+</div>
