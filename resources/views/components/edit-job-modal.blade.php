e0caab215b3cc217a523faee141e216e580bc46f
Ben Rogers <benrogers1299@outlook.com>
2022-04-10 14:30:52 +1000

    Seperate edit modal into blade component

---
+++ resources/views/components/edit-job-modal.blade.php
@@ -0,0 +1,57 @@
+@props(['job'])
+<div class="modal fade" id="editJobModal" tabindex="-1" aria-labelledby="editJobModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header bg-blue-custom">
+                <h5 class="modal-title text-white" id="editJobModalLabel">Edit Job Details</h5>
+                <button type="button" class="btn-close bg-light" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form action="{{ route("updatejob", $job->id) }}" method="POST" class="add-job-form">
+                    @csrf
+                    @method('put')
+
+                    <input type="hidden" name="action" value="add_job">
+                    <div class="d-flex flex-column mb-2">
+                        <label class="mb-1">Company:</label>
+                        <input class="form-control" type="text" name="company_name" maxlength="50" placeholder="Company Name" value="{{ $job->company_name }}" autofocus required>
+                    </div>
+
+                    <div class="d-flex flex-column mb-2">
+                        <label class="mb-1">Position:</label>
+                        <input class="form-control" type="text" name="position_title" maxlength="50" placeholder="Position Title" value="{{ $job->job_title }}" required>
+                    </div>
+
+                    <div class="d-flex flex-column mb-2">
+                        <label class="mb-1">Salary:</label>
+                        <input class="form-control" type="number" name="salary" maxlength="50" placeholder="Salary" value="{{ $job->salary }}" required>
+                    </div>
+
+                    <div class="d-flex flex-column mb-2">
+                        <label class="mb-1">Description:</label>
+                        <textarea class="form-control" name="description" id="description" cols="30" rows="7" required>{{ $job->description }}</textarea>
+                    </div>
+
+                    <div class="d-flex flex-column mb-2">
+                        <label class="mb-1">Post URL:</label>
+                        <input class="form-control" type="url" name="post_url" placeholder="https://example.com" value="{{ $job->post_url }}" required>
+                    </div>
+
+                    <div class="d-flex flex-column mb-2">
+                        <label class="mb-1">Status:</label>
+                        <select name="application_status" class="form-select">
+                            <option value="Sent" selected>Sent</option>
+                            <option value="Interviewing">Interviewing</option>
+                            <option value="Offer">Offer</option>
+                            <option value="Expired">Expired</option>
+                        </select>
+                    </div>
+
+                    <div class="modal-footer">
+                        <button type="submit" name="submit" class="btn btn-primary">Update</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
