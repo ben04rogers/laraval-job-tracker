aaaf74b2a441f373303ae60415772496137739be
Ben Rogers <47819009+ben04rogers@users.noreply.github.com>
2022-05-24 18:24:15 +1000

    Add mobile menu

--- public/js/main.js
+++ public/js/main.js
@@ -28,3 +28,29 @@ $(".image-upload-wrap").bind("dragover", function () {
 $(".image-upload-wrap").bind("dragleave", function () {
     $(".image-upload-wrap").removeClass("image-dropping");
 });
+
+// Mobile Navbar
+const mobileNavButton = document.querySelector('.hamburger-button');
+mobileNavButton.addEventListener('click', (event) => {
+    // Toggle mobile menu
+    document.querySelector('.mobile-nav').classList.toggle('hide-element');
+
+    // Toggle navbar icon
+    mobileNavButton.querySelector('.hamburger').classList.toggle('hide-element');
+    mobileNavButton.querySelector('.close').classList.toggle('hide-element');
+
+})
+
+window.addEventListener("resize", () => {
+    if (window.innerWidth > 1000) {
+        // Toggle mobile menu
+        let mobileNav = document.querySelector('.mobile-nav');
+
+        if (!mobileNav.classList.contains('hide-element')) {
+            mobileNav.classList.add('hide-element');
+            mobileNavButton.querySelector('.hamburger').classList.toggle('hide-element');
+            mobileNavButton.querySelector('.close').classList.toggle('hide-element');
+
+        }
+    }
+})
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,25 +1,35 @@
-<nav class="navbar navbar-expand-md navbar-dark border-bottom shadow-sm bg-white py-0">
+<nav class="navbar navbar-expand-md navbar-dark border-bottom shadow-sm bg-white">
     <div class="container">
         <div class="collapse navbar-collapse d-flex justify-content-between" id="navbarNav">
             <a class="navbar-brand py-0" href="{{ route("home") }}">
                 <img src="{{url('/assets/images/job-track-logo.svg')}}" class="logo">
             </a>
-            <ul class="navbar-nav d-flex">
-                <div class="d-flex">
-                    <li class="nav-item mx-3 list-unstyled">
-                        <a class="nav-link text-black py-3 {{ Request::path() ==  '/' ? 'active' : ''  }}" href="{{ route("home") }}"><i class="fas fa-home"></i> Home</a>
-                    </li>
-                    <li class="nav-item mx-3 list-unstyled">
-                        <a class="nav-link text-black py-3 {{ Request::path() ==  'dashboard' ? 'active' : ''  }}" href="{{ route("dashboard") }}"><i class="fas fa-columns"></i> Dashboard</a>
-                    </li>
-                    <li class="nav-item mx-3 list-unstyled">
-                        <a class="nav-link text-black py-3 {{ Request::path() ==  'files' ? 'active' : ''  }}" href="{{ route("files") }}"><i class="fas fa-file-pdf"></i> Files</a>
-                    </li>
-                    <li class="nav-item mx-3 list-unstyled">
-                        <a class="nav-link text-black py-3 {{ Request::path() ==  'calendar' ? 'active' : ''  }}" href="{{ route("calendar") }}"><i class="fas fa-calendar"></i> Calendar</a>
-                    </li>
-                </div>
-            </ul>
+
+            <button class="hamburger-button">
+                <i class="hamburger fas fa-bars fa-lg"></i>
+                <i class="close fas fa-times fa-lg hide-element"></i>
+            </button>
+
+            <div class="desktop-nav">
+                <ul class="navbar-nav d-flex">
+                    <div class="d-flex">
+                        <li class="nav-item mx-3 list-unstyled">
+                            <a class="nav-link text-black py-3 {{ Request::path() ==  '/' ? 'active' : ''  }}" href="{{ route("home") }}"><i class="fas fa-home"></i> Home</a>
+                        </li>
+                        <li class="nav-item mx-3 list-unstyled">
+                            <a class="nav-link text-black py-3 {{ Request::path() ==  'dashboard' ? 'active' : ''  }}" href="{{ route("dashboard") }}"><i class="fas fa-columns"></i> Dashboard</a>
+                        </li>
+                        <li class="nav-item mx-3 list-unstyled">
+                            <a class="nav-link text-black py-3 {{ Request::path() ==  'files' ? 'active' : ''  }}" href="{{ route("files") }}"><i class="fas fa-file-pdf"></i> Files</a>
+                        </li>
+                        <li class="nav-item mx-3 list-unstyled">
+                            <a class="nav-link text-black py-3 {{ Request::path() ==  'calendar' ? 'active' : ''  }}" href="{{ route("calendar") }}"><i class="fas fa-calendar"></i> Calendar</a>
+                        </li>
+                    </div>
+                </ul>
+            </div>
+
+            <div class="desktop-nav">
             {{-- Only show add button for logged in users --}}
             @auth
                 <ul class="my-0">
@@ -44,20 +54,39 @@
                     </div>
                 </ul>
             @endauth
+            </div>
 
 
             {{-- Only show links if user is a guest (has not logged in) --}}
             @guest
-                <div class="d-flex">
-                    <li class="nav-item list-unstyled">
-                        <a class="btn border-blue-custom mx-2" href="{{ route("login") }}"><i class="fas fa-sign-in-alt"></i> Login</a>
-                    </li>
-                    <li class="nav-item list-unstyled">
-                        <a class="btn bg-blue-custom text-white" href="{{ route("register") }}"><i class="fas fa-user-plus"></i> Register</a>
-                    </li>
+                <div class="desktop-nav">
+                    <div class="d-flex">
+                        <li class="nav-item list-unstyled">
+                            <a class="btn border-blue-custom mx-2" href="{{ route("login") }}"><i class="fas fa-sign-in-alt"></i> Login</a>
+                        </li>
+                        <li class="nav-item list-unstyled">
+                            <a class="btn bg-blue-custom text-white" href="{{ route("register") }}"><i class="fas fa-user-plus"></i> Register</a>
+                        </li>
+                    </div>
                 </div>
-                @endguest
-                </ul>
+            @endguest
+        </div>
+
+        <div class="mobile-nav rounded mt-2 hide-element">
+            <ul class="text-left px-0 my-0">
+                    <li class="nav-item list-unstyled">
+                        <a class="nav-link text-black py-3 text-white" href="{{ route("home") }}">Home</a>
+                    </li>
+                    <li class="nav-item list-unstyled">
+                        <a class="nav-link text-black py-3 text-white" href="{{ route("dashboard") }}">Dashboard</a>
+                    </li>
+                    <li class="nav-item list-unstyled">
+                        <a class="nav-link text-black py-3 text-white" href="{{ route("files") }}">Files</a>
+                    </li>
+                    <li class="nav-item list-unstyled">
+                        <a class="nav-link text-black py-3 text-white" href="{{ route("calendar") }}">Calendar</a>
+                    </li>
+            </ul>
         </div>
     </div>
 </nav>
