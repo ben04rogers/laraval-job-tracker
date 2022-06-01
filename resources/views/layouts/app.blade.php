80135adb5dbab12f4a29bffa66894e2b474e78e2
Ben Rogers <47819009+ben04rogers@users.noreply.github.com>
2022-06-01 20:54:40 +1000

    Remove turbolinks

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-    <script src="https://unpkg.com/turbolinks"></script>
+{{--    <script src="https://unpkg.com/turbolinks"></script>--}}
 
     <title>Job Application Tracker</title>
 </head>
